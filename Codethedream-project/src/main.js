039f23c7ae7091d0aed9b5fb044adf86c9cf47f3
bossuche9 <uchennaobi102@gmail.com>
2025-01-29 01:39:09 -0500

    ww

--- Codethedream-project/src/main.js
+++ Codethedream-project/src/main.js
@@ -29,26 +29,25 @@ fetch(dogurl, {
 
 function displayDogs(dogString){
   const dogs = dogString;
-  
 
-  for(let i =0; i < dogs.length-170; i++){
-    console.log(dogs[i]);
-    console.log(dogs[i].bred_for);
+  for(let i =0; i < dogs.length-168; i++){
+   // console.log(dogs[i]);
+    //console.log(dogs[i].bred_for);
    
     const dogName = document.createElement("p");
     const dogImage = document.createElement("img");
     const dogTemper = document.createElement("p");
+
     dogName.textContent = `Dog name: ${dogs[i].name}` ;
     dogImage.setAttribute("id",dogs[i].image.id);
     dogImage.setAttribute("src",`${dogs[i].image.url}`);
     dogTemper.textContent = `Dog Temperament: ${dogs[i].temperament}` ;
-    console.log(dogImage);
     Doglist.appendChild(dogName);
     Doglist.appendChild(dogImage);
     Doglist.appendChild(dogTemper);
   };
 
-  console.log(Doglist);
+
 } 
-      
+     
 
