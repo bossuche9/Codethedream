f0c8a3d79740365b97e646990b6b136ca6e1a9a8
bossuche9 <uchennaobi102@gmail.com>
2025-02-08 20:40:32 -0500

    added error checking to dogdetails.js

--- Codethedream-project/src/dogdetails.js
+++ Codethedream-project/src/dogdetails.js
@@ -1,29 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const storedDogs = localStorage.getItem("dogsData");
-  
-    
    
     if (storedDogs) {
       const allDogs = JSON.parse(storedDogs);
-      
+      populateTable(allDogs);
+
     } else {
       console.log("No dog data found. Please visit the home page first.");
     }
   });
-  const storedDogs = localStorage.getItem("dogsData");
-  const allDogs = JSON.parse(storedDogs);
-  console.log(allDogs[0]);
+ 
 
   function populateTable(dogs){
     const dogTable = document.querySelector(".dogtable")
 
-    for(let i = 0; i < allDogs.length ; i++){
+    for(let i = 0; i < dogs.length ; i++){
       const dogRow = document.createElement("tr");
       dogTable.appendChild(dogRow);
       const dogData1 = document.createElement("td");
       const dogData2  = document.createElement("td");
       const dogData3 =  document.createElement("td");
-      
+
       dogData1.textContent = dogs[i].name;
       dogData2.textContent =  `${dogs[i].weight.imperial} lbs / ${dogs[i].weight.metric} kg`;
       dogData3.textContent =  dogs[i].life_span;
@@ -34,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   }
 
-  populateTable(allDogs);
+  
 
 
  
